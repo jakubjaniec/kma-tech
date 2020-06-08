2820d12e29850fb8a7995037192d9ebf6d96eb6e
Milan_Krupa <milan.krupa9521@gmail.com>
2020-06-08 13:47:16 +0200

    Start work on shopping cart page with stylesheets
    I added just a very basic structure with styles for back button

---
+++ shoppingCart.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KOSZYK | KMA-TECH</title>
+    <link rel="shortcut icon" href="./imgs/kma-tech-icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="./styles/shoppingCart.css">
+</head>
+
+<body>
+    <a href="index.php" class="back-btn">
+        <div></div>
+    </a>
+    <section>
+
+    </section>
+    <section></section>
+
+    <script>
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+    </script>
+</body>
+
+</html>
